dfeb47587f3630667d126b10a0078718e2833ba9
Jawad Safwan <safwan.cerberus@gmail.com>
2023-10-10 15:50:44 +0100

    To be pushed to github

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,8 +141,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = 'True'
 
-EMAIL_HOST_USER = 'safwan.dvma@gmail.com'
-EMAIL_HOST_PASSWORD = 'bhnpvpmgplpomisy'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Allow PayPal Popups
 
