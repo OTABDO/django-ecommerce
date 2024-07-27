392389bd0b5d65accde2dd7ec579b997c067e5e7
OTABDO <150673072+OTABDO@users.noreply.github.com>
2024-07-28 00:17:55 +0100

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_extensions",
+
     # My Apps
     'store.apps.StoreConfig',
     'cart.apps.CartConfig',
